08dbf52a2655c798e477d48ef364eae7401504f0
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-05-12 22:55:34 -0300

    upsates the name of the contract to crud objects

--- lib/app/contracts/contract_data_crud.dart
+++ lib/app/contracts/contract_data_crud.dart
@@ -1,4 +1,4 @@
-abstract class ContractDataCrud {
+abstract class ContractDataCRUD {
   Future<void> create(Map<String, dynamic> data);
   Future<dynamic> read(String id);
   Future<void> update(String id, Map<String, dynamic> data);
